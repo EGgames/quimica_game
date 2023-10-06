45a741eeae23dab6ffe5fdecb87ffe7be56ce905
maximiliano <maximullermaxi1@gmail.com>
2023-10-06 12:25:10 -0300

    assets commit

---
+++ Assets/scripts/jefe-enemigo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jefe-enemigo : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
